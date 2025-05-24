0e4b3234ac4d64e6a8314fc7190ee9b62b33f3ac
hawciu <hawciu@gmail.com>
2025-05-24 23:22:23 +0200

    touch controls added

---
+++ Assets/Scripts/UI/ControlsTouchPosition.cs
@@ -0,0 +1,89 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ControlsTouchPosition : MonoBehaviour
+{
+    public Image l;
+    public Image r;
+    public Image u;
+    public Image d;
+
+    public Image marker;
+
+    public ControlHandler handler;
+
+    Bounds lb;
+    Bounds rb;
+    Bounds ub;
+    Bounds db;
+
+    private void Start()
+    {
+        CalculateControlsBounds();
+    }
+
+    private void Update()
+    {
+        CheckTouch();
+    }
+
+    private void CalculateControlsBounds()
+    {
+        lb = new Bounds(l.rectTransform.position, l.rectTransform.sizeDelta);
+        rb = new Bounds(r.rectTransform.position, r.rectTransform.sizeDelta);
+        ub = new Bounds(u.rectTransform.position, u.rectTransform.sizeDelta);
+        db = new Bounds(d.rectTransform.position, d.rectTransform.sizeDelta);
+    }
+
+    private void CheckTouch()
+    {
+        SetAllToNeutral();
+        if (Input.touchCount > 0)
+        {
+            for (int i = 0; i < Input.touchCount; i++)
+            {
+                Touch touch = Input.GetTouch(i);
+
+                if (lb.Contains(touch.position))
+                {
+                    l.color = new Color(1f, 1f, 1f, 1f);
+                    handler.SetInput(SteeringDirection.LEFT);
+                }
+
+                if (rb.Contains(touch.position))
+                {
+                    r.color = new Color(1f, 1f, 1f, 1f);
+                    handler.SetInput(SteeringDirection.RIGHT);
+                }
+
+                if (ub.Contains(touch.position))
+                {
+                    u.color = new Color(1f, 1f, 1f, 1f);
+                    handler.SetInput(SteeringDirection.ACCELERATE);
+                }
+
+                if (db.Contains(touch.position))
+                {
+                    d.color = new Color(1f, 1f, 1f, 1f);
+                    handler.SetInput(SteeringDirection.BRAKE);
+                }
+                marker.rectTransform.position = touch.position;
+            }
+
+        }
+    }
+
+    void SetAllToNeutral()
+    {
+        handler.SetInput(SteeringDirection.NEUTRAL);
+        handler.SetInput(SteeringDirection.FORWARD);
+        l.color = new Color(1f, 1f, 1f, 0.5f);
+        r.color = new Color(1f, 1f, 1f, 0.5f);
+        u.color = new Color(1f, 1f, 1f, 0.5f);
+        d.color = new Color(1f, 1f, 1f, 0.5f);
+        marker.rectTransform.position = new Vector2(Screen.width/2, Screen.height/2);
+    }
+}
---
+++ Assets/TESTTESTTEST/testpointerhandler.cs
@@ -0,0 +1,80 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class testpointerhandler : MonoBehaviour
+{
+    int frame = 0;
+    public Image img;
+    public Image marker;
+
+    public Image l;
+    public Image r;
+    public Image u;
+    public Image d;
+
+
+    Bounds lb; //= new Bounds(l.rectTransform.position, l.rectTransform.sizeDelta);
+    Bounds rb; //= new Bounds(r.rectTransform.position, r.rectTransform.sizeDelta);
+    Bounds ub; //= new Bounds(u.rectTransform.position, u.rectTransform.sizeDelta);
+    Bounds db;// = new Bounds(d.rectTransform.position, d.rectTransform.sizeDelta);
+
+    bool doonce = true;
+    private void Start()
+    {
+        lb = new Bounds(l.rectTransform.position, l.rectTransform.sizeDelta);
+        rb = new Bounds(r.rectTransform.position, r.rectTransform.sizeDelta);
+        ub = new Bounds(u.rectTransform.position, u.rectTransform.sizeDelta);
+        db = new Bounds(d.rectTransform.position, d.rectTransform.sizeDelta);
+    }
+
+    private void Update()
+    {
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+            marker.rectTransform.position = touch.position;
+            if (lb.Contains(touch.position))
+            {
+                l.color = new Color(1f, 1f, 1f, 1f);
+            }
+            else
+            {
+                l.color = new Color(1f, 1f, 1f, 0.5f);
+            }
+
+            if (rb.Contains(touch.position))
+            {
+                r.color = new Color(1f, 1f, 1f, 1f);
+            }
+            else
+            {
+                r.color = new Color(1f, 1f, 1f, 0.5f);
+            }
+
+            if (ub.Contains(touch.position))
+            {
+                u.color = new Color(1f, 1f, 1f, 1f);
+            }
+            else
+            {
+                u.color = new Color(1f, 1f, 1f, 0.5f);
+            }
+
+            if (db.Contains(touch.position))
+            {
+                d.color = new Color(1f, 1f, 1f, 1f);
+            }
+            else
+            {
+                d.color = new Color(1f, 1f, 1f, 0.5f);
+            }
+        }
+        else
+        {
+            print("No touch contacts");
+        }
+    }
+}
