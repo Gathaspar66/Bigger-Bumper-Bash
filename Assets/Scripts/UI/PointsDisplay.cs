568d0f1fe5a3fde5a8abfccb68cb16dc6f58f85b
Gathaspar66 <62515742+Gathaspar66@users.noreply.github.com>
2025-06-01 16:42:09 +0200

    Multiplier improvement (#26)
    * Added elements
    
    * fix

--- Assets/Scripts/UI/PointsDisplay.cs
+++ Assets/Scripts/UI/PointsDisplay.cs
@@ -4,6 +4,7 @@ public class PointsDisplay : MonoBehaviour
 {
     public DigitRowHandler digitRowHandler;
 
+
     public void SetDigits(int points)
     {
         digitRowHandler.UpdatePointsDisplay(points);
--- Assets/Scripts/UI/PointsMultiplierDisplay.cs
+++ Assets/Scripts/UI/PointsMultiplierDisplay.cs
@@ -1,12 +1,21 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PointsMultiplierDisplay : MonoBehaviour
 {
-
     public DigitRowHandler digitRowHandler;
+    public Image MaxMultiplierImage;
 
     public void UpdatePointsMultiplierDisplay(int multiplier)
     {
         digitRowHandler.UpdatePointsDisplay(multiplier);
+        if (multiplier == PointsManager.instance.maxMultplier)
+        {
+            MaxMultiplierImage.gameObject.SetActive(true);
+        }
+        else
+        {
+            MaxMultiplierImage.gameObject.SetActive(false);
+        }
     }
 }
