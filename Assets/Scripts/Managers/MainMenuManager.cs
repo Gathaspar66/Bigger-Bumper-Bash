9a84d30e8a4b8d1f1c8da08ec945f6a70aa00074
hawciu <hawciu@gmail.com>
2025-06-02 19:02:52 +0200

    main menu about button added (very ugly but works)

---
+++ Assets/Scripts/Managers/MainMenuManager.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuManager : MonoBehaviour
+{
+    public static MainMenuManager instance;
+
+    public GameObject aboutPanel;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    private void Start()
+    {
+        ActivateManagers();
+        UpdateHighScoreText();
+    }
+
+
+    public void ActivateManagers()
+    {
+        SoundManager.instance.Activate();
+    }
+    public DigitRowHandler digitRowHandler;
+
+    public void OnGameStartButtonPressed()
+    {
+        SceneManager.LoadScene("Level1");
+    }
+
+    private void UpdateHighScoreText()
+    {
+        if (PlayerPrefs.GetInt("highScore") == 0)
+        {
+            return;
+        }
+
+        int points = PlayerPrefs.GetInt("highScore");
+        digitRowHandler.UpdatePointsDisplay(points);
+    }
+
+    public void ToggleAboutPanel(bool showPanel)
+    {
+        aboutPanel.SetActive(showPanel);
+    }
+}
