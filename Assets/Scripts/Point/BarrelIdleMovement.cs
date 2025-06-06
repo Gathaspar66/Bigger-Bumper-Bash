6bd860207c76b4f3df57194ddaada5902d2c72a2
hawciu <hawciu@gmail.com>
2025-06-06 16:37:13 +0200

    rebase 1/4 add level 1

---
+++ Assets/Scripts/Point/BarrelIdleMovement.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public float speed = 1;
+    float rotationspeed = 0.5f;
+
+    Vector3 startPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPosition = transform.position;    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoveUpAndDown();
+        Rotate();
+    }
+
+    void MoveUpAndDown()
+    {
+        transform.position = new Vector3(transform.position.x, Mathf.PingPong(Time.time, 1), transform.position.z);
+    }
+
+    void Rotate()
+    {
+        transform.Rotate(Vector3.up * rotationspeed);
+    }
+}
