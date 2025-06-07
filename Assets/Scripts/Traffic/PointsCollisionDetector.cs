fc461d0a915a8783c4d7c8bdc3460882d3d8b1b4
hawciu <hawciu@gmail.com>
2025-06-08 00:53:39 +0200

    barrel break effect working

--- Assets/Scripts/Traffic/PointsCollisionDetector.cs
+++ Assets/Scripts/Traffic/PointsCollisionDetector.cs
@@ -8,6 +8,7 @@ public class PointsCollisionDetector : MonoBehaviour
         //explosionScript = GetComponent<Explosion>();
         PointsManager.instance.AddPoints(1000);
         //explosionScript.Explode(Vector3.forward);
+        EffectManager.instance.SpawnAnEffect(Effect.BARREL, transform.position);
         Destroy(gameObject);
     }
 }
