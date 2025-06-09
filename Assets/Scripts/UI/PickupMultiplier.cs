61686822c0b7e72028fa146e8e6cfd669b2a00db
hawciu <hawciu@gmail.com>
2025-06-09 19:29:37 +0200

    barrel visual fill added

--- Assets/Scripts/UI/PickupMultiplier.cs
+++ Assets/Scripts/UI/PickupMultiplier.cs
@@ -5,18 +5,21 @@ using UnityEngine.UI;
 public class PickupMultiplier : MonoBehaviour
 {
     public List<Sprite> barrelSprites = new();
+    public List<Sprite> barrelSpritesGray = new();
     public List<Sprite> barrelMultiplier = new();
     public Image barrelImage;
+    public Image barrelImageGray;
     public Image multiplierValueImage;
 
-    private void UpdatePickupVisual(int value)
+    public void UpdatePickupMultiplierVisual(int value)
     {
         multiplierValueImage.sprite = barrelMultiplier[value - 1];
         barrelImage.sprite = barrelSprites[value - 1];
+        barrelImageGray.sprite = barrelSpritesGray[value - 1];
     }
 
-    public void UpdatePickupMultiplier(int value)
+    public void UpdatePickupMultiplierFill(float value)
     {
-        UpdatePickupVisual(value);
+        barrelImage.fillAmount = value;
     }
 }
