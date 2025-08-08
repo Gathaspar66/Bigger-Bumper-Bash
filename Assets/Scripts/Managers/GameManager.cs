3515d76de87c4e00b8b82c4a228752071e995130
Gathaspar66 <lukasiewiczkacper@outlook.com>
2025-08-08 22:56:30 +0200

    Added a camera lower and 60 fps on phone

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -20,6 +20,8 @@ public class GameManager : MonoBehaviour
 
     private void Start()
     {
+        Application.targetFrameRate = 120;
+        //QualitySettings.vSyncCount = 0;
         ActivateManagers();
     }
 
